4f0a0ac7e903e0ca1a60db0fc1da387bf624cd38
IvoryCandy <deovant@gmail.com>
2018-01-13 02:11:00 -0500

    update utils

--- sub_pixel_CNN/utils.py
+++ sub_pixel_CNN/utils.py
@@ -1,45 +1,5 @@
-import os
 import sys
 import time
-
-import torch
-import torch.nn as nn
-import torch.nn.init as init
-
-
-def compute_mean_and_std(dataset):
-    # Compute mean and std value of dataset.
-    data_loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
-    mean = torch.zeros(3)
-    std = torch.zeros(3)
-    print('* Computing mean and std *')
-    for inputs, targets in data_loader:
-        for i in range(3):
-            mean[i] += inputs[:, i, :, :].mean()
-            std[i] += inputs[:, i, :, :].std()
-    mean.div_(len(dataset))
-    std.div_(len(dataset))
-    return mean, std
-
-
-def init_params(net):
-    # Init layer parameters
-    for m in net.modules():
-        if isinstance(m, nn.Conv2d):
-            init.kaiming_normal(m.weight, mode='fan_out')
-            if m.bias:
-                init.constant(m.bias, 0)
-        elif isinstance(m, nn.BatchNorm2d):
-            init.constant(m.weight, 1)
-            init.constant(m.bias, 0)
-        elif isinstance(m, nn.Linear):
-            init.normal(m.weight, std=1e-3)
-            if m.bias:
-                init.constant(m.bias, 0)
-
-
-_, term_width = os.popen('stty size', 'r').read().split()
-term_width = int(term_width)
 
 
 TOTAL_BAR_LENGTH = 80
@@ -48,6 +8,7 @@ BEGIN_T = LAST_T
 
 
 def progress_bar(current, total, msg=None):
+
     global LAST_T, BEGIN_T
     if current == 0:
         BEGIN_T = time.time()  # Reset for new bar.
@@ -55,6 +16,7 @@ def progress_bar(current, total, msg=None):
     current_len = int(TOTAL_BAR_LENGTH * (current + 1) / total)
     rest_len = int(TOTAL_BAR_LENGTH - current_len) - 1
 
+    sys.stdout.write(' %d/%d' % (current + 1, total))
     sys.stdout.write(' [')
     for i in range(current_len):
         sys.stdout.write('=')
@@ -75,13 +37,6 @@ def progress_bar(current, total, msg=None):
 
     msg = time_used
     sys.stdout.write(msg)
-    for i in range(term_width - int(TOTAL_BAR_LENGTH) - len(msg) - 3):
-        sys.stdout.write(' ')
-
-    # Go back to the center of the bar.
-    for i in range(term_width - int(TOTAL_BAR_LENGTH / 2) + 3):
-        sys.stdout.write('\b')
-    sys.stdout.write(' %d/%d ' % (current + 1, total))
 
     if current < total - 1:
         sys.stdout.write('\r')
